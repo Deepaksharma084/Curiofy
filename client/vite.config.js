8cff84e9ed071ef894a294bee78d69e6b58a6c2a
Deepaksharma084 <dk3433045@gmail.com>
2025-06-14 10:46:27 +0530

    updated index.js and vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    port: 5173
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path'); // Add this line at the top
 const indexRouter = require("./routes/indexRouter");
 const blogRoute = require("./routes/blogRoute");
 const ownerRoute = require("./routes/ownerRoute");
@@ -30,6 +31,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/blogs", blogRoute);
 app.use("/owner", ownerRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(3000, '0.0.0.0', () => {
     console.log("Server is running on port 3000");
