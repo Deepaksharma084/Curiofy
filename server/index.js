fe7292974ff6a5cdf79a371e3be77bd6b30293ed
Deepaksharma084 <dk3433045@gmail.com>
2025-07-13 11:31:35 +0530

    refactor: configure backend as API-only server with production CORS ,that is removed express static logic from index.js file

--- server/index.js
+++ server/index.js
@@ -60,12 +60,6 @@ app.use("/blogs", blogRoute);
 app.use("/owner", ownerRoute);
 app.use('/ai', aiRoute);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
