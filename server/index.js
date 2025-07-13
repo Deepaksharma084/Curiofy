9176170d9673b4b419a51188d37618aa1b65ca03
Deepaksharma084 <dk3433045@gmail.com>
2025-07-13 14:43:47 +0530

    changed render yaml file and simplified index.js file

--- server/index.js
+++ server/index.js
@@ -14,47 +14,18 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
-mongoose.connect(process.env.MONGO_URI, {
-}).then(() => {
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('MongoDB connection error:', err);
 });
 
-// Defining the origins that are allowed to access backend
-const allowedOrigins = [
-    'https://curiofy.onrender.com',
-    'http://localhost:5173',
-    'http://192.168.1.8:5173'
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Expires']
-};
-
-// This is the most important part for preflight requests.
-// It tells Express to handle OPTIONS requests globally and apply CORS.
-app.options('*', cors(corsOptions));
-
-// using the CORS middleware for all other requests.
-app.use(cors(corsOptions));
-
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/blogs", blogRoute);
 app.use("/owner", ownerRoute);
